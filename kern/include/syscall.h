71c8bcc033c9e882add0c039d031f3fee6e798dd
asucena <asucena.digiovanni@mail.utoronto.ca>
2020-04-13 17:14:38 -0400

    syscall.h updated

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -15,6 +15,6 @@ int sys_fork(struct trapframe *tf, int *retval);
 int sys_getpid(int *retval);
 int sys_waitpid(pid_t pid, int* status, int options, int *retval);
 int sys_execv(const char *program, char **args);
-//void sys__exit(int exitcode);
+int sys_sbrk(intptr_t ammount, int* retval);
 
 #endif /* SYSCALL_H */
